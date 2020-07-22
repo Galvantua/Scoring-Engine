7ba6b44e9ddfe225703c0d98a00260d48f1d4acd
Galvantua <34282639+Galvantua@users.noreply.github.com>
2020-07-22 01:54:12 -0400

    formattttt

--- engine.sh
+++ engine.sh
@@ -11,8 +11,8 @@ init () {
 	touch "$scoringNegatives"
 	touch "$scoringPositives"
 	echo "This is the scoring report for the assesment for the St. Augustine Composite Squadron Cyber Education Program." > "$scoringReport"
-	echo " Points Earned" >> "$totalScore"
-	cat "$totalScore" >> "$scoringReport"
+	
+	echo "\t\t\t Points Earned" | cat "$totalScore" - >> "$scoringReport"
 	cat "$scoringNegatives" >> "$scoringReport"
 	cat "$scoringPositives" >> "$scoringReport"
 	echo "" > $scoringPositives
