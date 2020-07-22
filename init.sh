4b21b74a24e4d7d09576002dff6d8b24df3b7761
Galvantua <34282639+Galvantua@users.noreply.github.com>
2020-07-22 15:42:17 -0400

    create first init script functions plus test

--- init.sh
+++ init.sh
@@ -3,6 +3,26 @@
 ###### Init Functions ######
 
 createVuln() {
+	type="$1"
+	points=$2
+	var1="$3"
+	test=$($type "test" $var1)
+	message=$($type "message" $var1 )
+	echo " 
+if [ ${test} ]; then
+	scorePoints \"$points\" \"$message\"
+fi
+"
+}
+
+deleteUser() {
+	outputType="$1"
+	user="$2"
+	if [ "$outputType" = "test" ]; then
+		return "getent passwd $user"
+	elif [ "$outputType" = "message" ]; then
+		return "Deleted User $user"
+	fi
 	
 }
 ######
@@ -10,3 +30,6 @@ createVuln() {
 echo " Welcome to the init script for the St Augustine Composite Squadron Scoring Engine"
 
 echo "user accts"
+
+
+createVuln "deleteUser" 3 "eve"
