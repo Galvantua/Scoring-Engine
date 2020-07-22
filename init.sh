f715b11eca67dd5ba7f913e342c74b6b1ea29de7
Galvantua <34282639+Galvantua@users.noreply.github.com>
2020-07-22 15:26:22 -0400

    make init script file

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+###### Init Functions ######
+
+createVuln() {
+	
+}
+######
+
+echo " Welcome to the init script for the St Augustine Composite Squadron Scoring Engine"
+
+echo "user accts"
