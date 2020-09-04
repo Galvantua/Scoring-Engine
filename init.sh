fceddfcc824dea867ddfb809bec9f14355825900
Galvantua <34282639+Galvantua@users.noreply.github.com>
2020-09-04 15:55:33 -0400

    destroy unneeded echos to engine

--- init.sh
+++ init.sh
@@ -210,16 +210,11 @@ totalpoints=0
 ###### Start Init ######
 timedatectl set-timezone America/New_York
 clear
-echo "Welcome to the init script for the St Augustine Composite Squadron Scoring Engine"
-echo ""
-echo "Creating engine..."
-touch engine.sh
-touch otherStuff
-touch vulns
-echo "" > vulns
-echo ""
+echo "Welcome to the init script for the St Augustine Composite Squadron Scoring Engine" #replace with gui
+
 promptText "What is the System admin user?"
 sysUser="$userResponse"
+
 if [ "$(grep 'bash /opt/Scoring-Engine/engine.sh' /etc/crontab)" = "" ]; then
 	echo "DISPLAY=:0.0" >> /etc/crontab
 	echo "XAUTHORITY=/home/${sysUser}/.Xauthority" >> /etc/crontab
@@ -228,92 +223,7 @@ fi
 touch /etc/lightdm/lightdm.conf
 echo "[Seat:*]
 autologin-user=${sysUser}" > /etc/lightdm/lightdm.conf
-echo '#!/bin/bash
-# If you have this VM as an assesment leave NOW!!!
-# Looking through this file is a violation of integrety, 
-# and I WILL find out!!! 
 
-
-####### init functions #######
-init () {' > otherStuff
-echo "
-	cp /opt/Scoring-Engine/README /home/"${sysUser}"/Desktop/README
-" >> otherStuff
-echo '
-	touch "$scoringReport"
-	touch "$totalScore"
-	touch "$scoringNegatives"
-	touch "$scoringPositives"
-	touch "$fixedVulns"
-	touch "$lastScore"
-
-	score=$(cat "$totalScore")
-	score="${score} Points Earned out of ${totalPoints}"
-	numberVulns=$(cat "$fixedVulns")
-
-	penalties=$(cat "$scoringNegatives" )
-	vulns=$(cat "$scoringPositives" )
-	
-	cat "/opt/Scoring-Engine/head" > "$scoringReport"
-	echo 	"<h1>This is the scoring report for the assesment for the St. Augustine Composite Squadron Cyber Education Program.</h1>" >> "$scoringReport"
-
-	echo	"<br />" >> "$scoringReport"
-
-	echo	"<h2> Updated at $(date) </h2>" >> "$scoringReport"
-	echo	"<h2> $score </h2>" >> "$scoringReport"
-	
-	echo	"<h3> Penalties: </h3>" >> "$scoringReport"
-
-	echo	"$penalties" >> "$scoringReport"
-
-	echo	"<h3> Fixed Vulnerabilities: </h3>" >> "$scoringReport"
-	echo	"<h3> $numberVulns fixed out of $totalVulns </h3>" >> "$scoringReport"
-
-	echo	"$vulns" >> "$scoringReport"
-	echo 	"</body></html>" >> "$scoringReport"
-	echo "" > "$scoringPositives"
-	echo "" > "$scoringNegatives"
-	echo "$(cat "$totalScore")" > "$lastScore"
-	echo "" > "$totalScore"
-	echo "" > "$fixedVulns"
-}' >> otherStuff
-
-
-echo 'scorePoints () {
-	#$1 Points
-	#$2 Message
-	score=$(cat $totalScore)
-	newScore=$(($score + $1))
-	echo "<p class=\"vulns\">$2 : <span class=\"green\">$1 pts</span></p>" >> "$scoringPositives"
-	echo $newScore > "$totalScore"
-	fixed=$(cat $fixedVulns)
-	newFixed=$(($fixed + 1))
-	echo "$newFixed" > $fixedVulns
-}
-
-removePoints () {
-	#$1 Points
-	#$2 Message
-	score=$(cat "$totalScore")
-	newScore=$(($score - $1))
-	echo "<p class=\"penalties\">$2 : <span class=\"red\">$1 pts</span></p>" >> "$scoringNegatives"
-	echo $newScore > "$totalScore"
-}
-sendGainedPoints(){
-	notify-send -u critical -i "/opt/Scoring-Engine/gained.png" "Scoring Engine" "You Gained Points"
-	aplay "/opt/Scoring-Engine/gained.wav"
-}
-sendLostPoints(){
-	notify-send -u critical -i "/opt/Scoring-Engine/lost.png" "Scoring Engine" "You Lost Points"
-	aplay "/opt/Scoring-Engine/lost.wav"
-}
-' >> otherStuff
-
-
-
-echo "Engine init done"
-echo ""
-echo "Forensics Questions"
 
 #### Forensics Questions
 #test if user wants to add forensics questions
@@ -337,10 +247,6 @@ done
 if [ "$userResponse" == "Y" ]; then
 	vulnMakerUI "Would you like to add a Forensics Question?" "createForensics" "What question should be answered?" "What is the answer to the question?" "What should the file be named? (We reccomend \"Forensics_Question_number\")"
 fi
-
-
-echo ""
-echo "User Accounts"
 
 ##### User accounts
 #test if user wants to do the user accts section
@@ -401,42 +307,3 @@ if [ "$userResponse" == "Y" ]; then
 	done
 	
 fi
-
-echo "
-####### Init Vars #######
-
-scoringReport=\"/home/"${sysUser}"/Desktop/Score Report.html\"
-scoringNegatives=\"/opt/Scoring-Engine/penalties\"
-scoringPositives=\"/opt/Scoring-Engine/gainedVulns\"
-totalScore=\"/opt/Scoring-Engine/totalScore\"
-lastScore=\"/opt/Scoring-Engine/lastScore\"
-fixedVulns=\"/opt/Scoring-Engine/fixedVulns\"
-
-totalVulns=${totalvulns}
-totalPoints=${totalpoints}
-
-####### Run Script #######
-init" >> otherStuff
-
-
-cat otherStuff > engine.sh
-
-cat vulns >> engine.sh
-
-echo '
-if [[ "$(cat $totalScore)" = "" ]]; then
-	echo 0 > "$totalScore"
-fi
-
-if [[ "$(cat $fixedVulns)" = "" ]]; then
-	echo 0 > "$fixedVulns"
-fi
-
-if [[ "$( cat "$totalScore")" -gt "$(cat "$lastScore")" ]]; then
-	sendGainedPoints
-elif [[ "$( cat "$totalScore")" -lt "$(cat "$lastScore")" ]]; then
-	sendLostPoints
-fi
-' >> engine.sh
-
-
